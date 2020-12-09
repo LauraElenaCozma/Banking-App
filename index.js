7d70219ceade347388324bccfea578c6bef0fc09
unknown <cozmalaura23@gmail.com>
2020-12-09 22:42:10 +0200

    Added GraphQl schema for Users, Accounts and Promotions

--- index.js
+++ index.js
@@ -3,60 +3,146 @@ const app = express();
 const port = 3000;
 
 const models = require('./models');
+const {graphqlHTTP} = require('express-graphql');
+const {buildSchema} = require('graphql');
+
+
+
+const schema = buildSchema(`
+type User {
+    id: Int
+    firstName: String
+    lastName: String
+    phone: String
+    email: String
+    password: String
+    createdAt: String
+    updatedAt: String
+}
+type Account {
+    id: Int
+    userId: Int
+    iban: String
+    balance: Float 
+    blocked: Boolean
+    createdAt: String
+    updatedAt: String
+}
+type Promotion {
+    id: Int
+    name: String
+    price: Float
+    startDate: String
+    endDate: String
+    maxNoOfAccounts: Int
+    maxSumOfTransactions: Int
+    createdAt: String
+    updatedAt: String
+}
+type UserPromotion {
+    id: Int
+    userId: Int
+    promotionId: Int
+    createdAt: String
+    updatedAt: String
+}
+type Query {
+    user(userId: Int!): User
+    account(userId: Int!): [Account]
+    promotion(promotionId: Int!): Promotion
+    promotionsOfUser(userId: Int!): [Promotion]
+}
+type Mutation {
+    createUserPromotion(userId: Int, promotionId: Int): UserPromotion
+}`);
+
+const rootValue = {
+    user: async ({userId}) => {
+        const user = await models.User.findByPk(userId);
+        return user;
+    },
+    /*query GetUser{
+  user(userId: 3){
+    id
+    firstName
+    lastName
+    email
+    password
+    createdAt
+    updatedAt
+  }
+}*/
+    account: async ({userId}) => {
+        const user = await models.User.findByPk(userId);
+        const accounts = await user.getAccounts();
+        return accounts;
+    },
+    /*query GetAccountsOfUser{
+  account(userId:3){
+    iban
+    balance
+    blocked
+  }
+}*/
+    promotionsOfUser: async ({userId}) => {
+        //get all the promotions of an user
+        const user = await models.User.findByPk(userId);
+        const promotions = await user.getPromotions();
+
+        return promotions;
+    },
+    /*query GetPromotionsOfUser{
+  promotionsOfUser(userId:3) {
+    name
+    price
+    startDate
+    endDate
+    maxNoOfAccounts
+    maxSumOfTransactions
+  }
+}*/
+    createUserPromotion: async ({userId, promotionId}) => {
+        const user = await models.User.findByPk(userId);
+        const promotion = await models.Promotion.findByPk(promotionId);
+        await user.addPromotion(promotion);	
+        //what should be returned?
+        //TODO: change return into the added userPromotion
+        return userPromotion;
+    },
+    /*query GetPromotionsOfUser{
+  promotionsOfUser(userId:3) {
+    name
+    price
+    startDate
+    endDate
+    maxNoOfAccounts
+    maxSumOfTransactions
+  }
+}
+*/
+    promotion: async ({promotionId}) => {
+        const promotion = await models.Promotion.findByPk(promotionId); 
+        return promotion;
+    },
+    /*query GetPromotion {
+  promotion(promotionId: 4) {
+    id
+    name
+    price
+    startDate
+    endDate
+    maxNoOfAccounts
+    maxSumOfTransactions
+  }
+}*/
+}
+
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue,
+}));
 
 app.listen(port, () => {
     console.log('Server started!')
-});
-
-app.get('/hello-world', (req, res) => {
-    res.send('Your banking app has started!')
-});
-
-app.get('/users/:userId', async function(req, res) {
-    //get an user by id
-    const userId = req.params.userId;
-    const user = await models.User.findByPk(userId);
-    console.log(user);
-    res.send({
-        firstName: user.firstName,
-        lastName: user.lastName,
-        phone: user.phone,
-        email: user.email
-    })
-});
-
-
-app.get('/users/:userId/account', async function(req, res) {
-    const userId = req.params.userId;
-    const user = await models.User.findByPk(userId);
-    const account = await user.getAccounts();
-
-    res.send({
-      last_name: user.lastName,
-      account,
-    });
-  });
-
-
-app.get('/promotions/:promotionId', async function(req, res) {
-    const promotionId = req.params.promotionId;
-    const promotion = await models.Promotion.findByPk(promotionId); 
-    res.send({
-        name: promotion.name,
-        price: promotion.price,
-    })
-});
-
-app.get('/users/:userId/promotions', async function(req, res) {
-    //get all the promotions of a user
-    const userId = req.params.userId;
-    const user = await models.User.findByPk(userId);
-    const promotions = await user.getPromotions();
-    const countProm = await user.countPromotions();
-
-    res.send({
-        noOfPromotions: countProm,
-        promotions
-    });
-
 });
